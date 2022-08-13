9237da8c9492c303c0941bd002753380d8a1c8d6
Katja <katja.slangewal@gmail.com>
2022-08-12 20:13:30 -0400

    Added figure to callback

--- dm_callback.m
+++ dm_callback.m
@@ -1,21 +1,26 @@
 function dm_callback(src,evt,varargin)
-   % in myUserFcn.m somewhere on the matlab path.
 
-   disp(['Got Event: ' evt.EventName]); % prints the event
-   disp(src);                           % prints the source object (UserFunctions)
-   disp(src.hSI);                       % each source object has access to the main
+%    disp(['Got Event: ' evt.EventName]); % prints the event
+%    disp(src);                           % prints the source object (UserFunctions)
+%    disp(src.hSI);                       % each source object has access to the main
+% 
+%                                         % ScanImage® Model object
+%    disp(src.hSI.imagingSystem);          % from which we can access ScanImage
+%                                         % methods and properties
 
-                                        % ScanImage® Model object
-   disp(src.hSI.imagingSystem)          % from which we can access ScanImage
-                                        % methods and properties
-
-   image = src.hSI.hDisplay.lastFrame{1};
+    image = src.hSI.hDisplay.lastFrame{1};
    
-   f = log(abs(fftshift(fft2(image)))+1);
-   pol = ImToPolar(f, 0, .8, 300, 50);
-   meanpol = mean(pol,2);
-   metric = mean(meanpol(10:40));
+    f = log(abs(fftshift(fft2(image)))+1);
+    pol = ImToPolar(f, 0, .8, 300, 50);
+    meanpol = mean(pol,2);
+    metric = mean(meanpol(10:40));
 
-   judp('SEND', 10022,'localhost', cast(num2str(metric), 'int8'));
-
+    figure(1022);
+    subplot(1,2,1);
+    imshow(pol, []);
+    subplot(1,2,2);
+    plot(meanpol);
+       
+    disp("Sending frame")
+    judp('SEND', 10022,'localhost', cast(num2str(metric), 'int8'));
 end
